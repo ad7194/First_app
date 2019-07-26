765386213c4243d3231055de8de2e4c026586655
Aditya <adityasharma711994@gmail.com>
2019-07-26 20:12:28 +0530

    little change in provider

---
+++ lib/providers/products.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+//Need to define a list of product for that need to import product modal
+//import '../models/product.dart';
+
+class Products with ChangeNotifier {
+  //List<Products> _items = [];
+}
